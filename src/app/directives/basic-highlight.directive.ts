3e329db5f4f94f59a4cb82c441557014b2360a38
Zoran Markovic <zoran.markovic@netcentric.biz>
2017-04-12 00:02:12 +0200

    create custom directive and use HostListener

--- src/app/directives/basic-highlight.directive.ts
+++ src/app/directives/basic-highlight.directive.ts
@@ -9,13 +9,4 @@ export class BasicHighlightDirective implements OnInit {
     ngOnInit() {
         this.elementRef.nativeElement.style.backgroundColor = 'green';
     }
-
-    /*
-    In the last lecture, we used the Angular Renderer class to change the style of a HTML element.
-    As explained in that lecture, you should use the Renderer for any DOM manipulations.
-
-    Of course, you can do more than simply change the styling of an element via setStyle().
-    Learn more about the available Renderer methods here:
-    https://angular.io/docs/ts/latest/api/core/index/Renderer2-class.html
-     */
 }
--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2, OnInit} from '@angular/core';
+import {Directive, ElementRef, Renderer2, OnInit, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,7 +8,29 @@ export class BetterHighlightDirective implements OnInit{
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
   }
+
+  /*
+  In the last lecture, we used the Angular Renderer class to change the style of a HTML element.
+  As explained in that lecture, you should use the Renderer for any DOM manipulations.
+
+  Of course, you can do more than simply change the styling of an element via setStyle().
+  Learn more about the available Renderer methods here:
+  https://angular.io/docs/ts/latest/api/core/index/Renderer2-class.html
+   */
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
+      this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+      this.elementRef.nativeElement.style.backgroundColor = 'lightgreen';
+  }
+
+  /*
+  @HostListener je listener directiva koja moze da bude bilo koji event,
+  ime metode je proizvoljno
+   */
 
 }
